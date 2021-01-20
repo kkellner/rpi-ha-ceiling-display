5fb4c9b397a6e493ca1e08409b404e55cf7e13a4
Kurt Kellner <kurt.kellner@gmail.com>
2021-01-20 08:48:43 -0700

    update wifi monitoring scripts

--- monitor_network.sh
+++ monitor_network.sh
@@ -4,7 +4,6 @@
 # */1 * * * * /home/kkellner/rpi-ha-ceiling-display/monitor_network.sh >> /var/log/wifi_monitor.log 2>&1
 
 pingable=$(DEFAULT_ROUTE=$(ip route show default | awk '/default/ {print $3}'); ping -q -c 1 $DEFAULT_ROUTE 2>&1 >/dev/null; echo $?)
-wifiInfo=$(nmcli --escape no --get-values IN-USE,CHAN,SIGNAL,RATE,BSSID dev wifi)
 
 now=$(date)
 if [ "$pingable" -gt  0 ]
@@ -15,5 +14,6 @@ then
     nmcli general status
 
 else
+    wifiInfo=$(nmcli --wait 10 --escape no --get-values ACTIVE,CHAN,SIGNAL,RATE,BSSID dev wifi | grep yes)
     echo "$now Gateway OK wifi: $wifiInfo"
 fi
